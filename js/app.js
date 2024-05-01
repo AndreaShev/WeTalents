a9fa85d433a55675162fd261559c73898285d39c
AndreaShev <104580679+AndreaShev@users.noreply.github.com>
2024-05-01 11:38:47 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3582,49 +3582,46 @@
     moveButton.addEventListener("click", (function() {
         if (window.innerWidth < 767) contentBlock.classList.toggle("animate");
     }));
-    const emailInput = document.getElementById("email");
-    const emailInputContainer = document.querySelector(".email-we-talented__input");
-    const indicator = document.querySelector(".indicator");
-    const successMessage = document.getElementById("successMessage");
-    const errorMessage = document.getElementById("errorMessage");
-    emailInput.addEventListener("input", (function() {
-        const email = emailInput.value;
-        if (email) indicator.classList.add("show-indicator"); else indicator.classList.remove("show-indicator");
+    window.addEventListener("DOMContentLoaded", (function() {
+        let emailForm = document.getElementById("email-form");
+        emailForm.addEventListener("submit", (function(event) {
+            event.preventDefault();
+            let emailInput = document.getElementById("email");
+            let email = emailInput.value;
+            let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            let isValidEmail = emailRegex.test(email);
+            if (isValidEmail) emailInput.value = "";
+        }));
+        let emailInput = document.getElementById("email");
+        let indicator = document.querySelector(".indicator");
+        emailInput.addEventListener("input", (function() {
+            let email = emailInput.value;
+            if (email) indicator.classList.add("show-indicator"); else indicator.classList.remove("show-indicator");
+        }));
+        indicator.addEventListener("click", validateEmail);
+        emailInput.addEventListener("keydown", (function(event) {
+            const key = event.key;
+            if (key === "Enter") validateEmail();
+        }));
+        function validateEmail() {
+            let emailInput = document.getElementById("email");
+            let email = emailInput.value;
+            let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            let isValidEmail = emailRegex.test(email);
+            let successMessage = document.getElementById("successMessage");
+            let errorMessage = document.getElementById("errorMessage");
+            let emailInputContainer = document.querySelector(".email-we-talented__input");
+            if (isValidEmail) {
+                successMessage.style.display = "block";
+                errorMessage.style.display = "none";
+                emailInputContainer.style.display = "none";
+            } else {
+                successMessage.style.display = "none";
+                errorMessage.style.display = "block";
+                emailInput.style.color = "#da3832";
+            }
+        }
     }));
-    emailInput.addEventListener("keydown", (function(event) {
-        const key = event.key;
-        if (key === "Enter") validateAndShowMessage();
-    }));
-    indicator.addEventListener("click", (function() {
-        validateAndShowMessage();
-    }));
-    emailInput.addEventListener("focusout", (function() {
-        clearMessages();
-    }));
-    function validateAndShowMessage() {
-        const email = emailInput.value;
-        if (validateEmail(email)) showSuccessMessage(); else showErrorMessage();
-    }
-    function validateEmail(email) {
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(email);
-    }
-    function showSuccessMessage() {
-        emailInputContainer.style.display = "none";
-        successMessage.style.display = "block";
-        errorMessage.style.display = "none";
-    }
-    function showErrorMessage() {
-        successMessage.style.display = "none";
-        errorMessage.style.display = "block";
-        emailInput.style.color = "#da3832";
-    }
-    function clearMessages() {
-        successMessage.style.display = "none";
-        errorMessage.style.display = "none";
-        emailInput.style.color = "";
-        indicator.classList.remove("show-indicator");
-    }
     window["FLS"] = true;
     isWebp();
 })();
